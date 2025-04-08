58c4db3f634f280e7acc482eb769d5ecec4b015c
Artem Lehotskyy <252810@edu.p.lodz.pl>
2025-04-08 20:45:25 +0200

    wniosłem ponowne poprawy do kodu

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -6,22 +6,18 @@ import edu.kis.vh.nursery.HanoiRhymer;
 
 public class DefaultRhymersFactory implements RhymersFactory {
 
-	@Override
 	public DefaultCountingOutRhymer GetStandardRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
 
-	@Override
 	public DefaultCountingOutRhymer GetFalseRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
 
-	@Override
 	public FIFORhymer GetFIFORhymer() {
 		return new FIFORhymer();
 	}
 
-	@Override
 	public HanoiRhymer GetHanoiRhymer() {
 		return new HanoiRhymer();
 	}
